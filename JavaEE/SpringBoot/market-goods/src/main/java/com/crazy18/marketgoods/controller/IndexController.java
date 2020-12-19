e9b61b4f4227546978d87a3913c628815ac40c7e
crazy 18 <1099461301@qq.com>
2020-12-19 16:40:28 +0800

    springbootDemo

---
+++ JavaEE/SpringBoot/market-goods/src/main/java/com/crazy18/marketgoods/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.crazy18.marketgoods.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+
+    @RequestMapping("/")
+//    @ResponseBody
+    String index(){
+        return "index";
+    }
+}
---
+++ JavaEE/SpringBoot/market-goods/src/main/java/com/crazy18/marketgoods/pojo/entity/Goods.java
@@ -0,0 +1,91 @@
+package com.crazy18.marketgoods.pojo.entity;
+
+import java.io.Serializable;
+
+/**
+ * goods
+ * @author 
+ */
+public class Goods implements Serializable {
+    private Integer goodsId;
+
+    private String title;
+
+    private String img;
+
+    private Integer goodTypeId;
+
+    private Double oldPrice;
+
+    private Double price;
+
+    private String onsale;
+
+    private Integer detail;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(Integer goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public Integer getGoodTypeId() {
+        return goodTypeId;
+    }
+
+    public void setGoodTypeId(Integer goodTypeId) {
+        this.goodTypeId = goodTypeId;
+    }
+
+    public Double getOldPrice() {
+        return oldPrice;
+    }
+
+    public void setOldPrice(Double oldPrice) {
+        this.oldPrice = oldPrice;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getOnsale() {
+        return onsale;
+    }
+
+    public void setOnsale(String onsale) {
+        this.onsale = onsale;
+    }
+
+    public Integer getDetail() {
+        return detail;
+    }
+
+    public void setDetail(Integer detail) {
+        this.detail = detail;
+    }
+}
