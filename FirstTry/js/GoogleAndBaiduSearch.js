d21822bf1c4687cf76e63b645d249ab89b40dc97
crazy18 <1099461301@qq.com>
2021-12-27 14:16:42 +0800

    self web

--- FirstTry/js/GoogleAndBaiduSearch.js
+++ FirstTry/js/GoogleAndBaiduSearch.js
@@ -1,12 +1,15 @@
 function google() {
+    var inputs = document.getElementsByTagName('input');
+    var act = "https://www.google.com/search?q="+inputs[0].value;
+    // document.search.action = act;
 
-    document.search.action="https://www.google.com/search";
-    document.search.submit();
+    // document.search.submit();
+    window.location.href = act;
 }
 
 
 function baidu() {
 
-    document.search.action="https://www.baidu.com/s";
+    document.search.action="https://www.baidu.com/s?wd=";
     document.search.submit();
 }
